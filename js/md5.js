420730075f5504d3552a687ff16fea4060f7f4c4
Ani <asridha1@andrew.cmu.edu>
2015-05-21 11:24:11 -0400

    debugging md5.js #0

--- js/md5.js
+++ js/md5.js
@@ -205,6 +205,7 @@ var md5 = (function(){
   {
     var bin = Array();
     var mask = (1 << chrsz) - 1;
+    console.log(str)
     for(var i = 0; i < str.length * chrsz; i += chrsz)
       bin[i>>5] |= (str.charCodeAt(i / chrsz) & mask) << (i%32);
     return bin;
